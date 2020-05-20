cb4ce1d96e085ef497710c929ebdaa6c56641842
Josef Veselý <49988965+JosefVesely@users.noreply.github.com>
2020-05-20 13:29:13 +0200

    Update and rename linear_sort.cpp to linear_search.cpp

---
+++ linear_search.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void linear_search(vector<int> numbers, int value)
+{
+    bool found;
+    int pos;
+
+    for(int x = 0; x < numbers.size(); x++) {
+        if(numbers[x] == value) {
+            found = true;
+            pos = x + 1;
+            break;
+        }
+	}
+
+	if (found == true) {
+        cout << "Element found at position " << pos;
+	}
+	else {
+        cout << "Element not found";
+	}
+}
+
+
+int main()
+{
+	vector<int> numbers = {8, 3, 20, 56, 42, 33};
+
+    linear_search(numbers, 42);
+
+	return 0;
+}

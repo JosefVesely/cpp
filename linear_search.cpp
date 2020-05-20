a85cc752c647a47332a4e187fd0c03a69ecf4613
Josef Veselý <49988965+JosefVesely@users.noreply.github.com>
2020-05-20 13:30:18 +0200

    Update linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -14,22 +14,22 @@ void linear_search(vector<int> numbers, int value)
             pos = x + 1;
             break;
         }
-	}
+    }
 
-	if (found == true) {
+    if (found == true) {
         cout << "Element found at position " << pos;
-	}
-	else {
+    }
+    else {
         cout << "Element not found";
-	}
+    }
 }
 
 
 int main()
 {
-	vector<int> numbers = {8, 3, 20, 56, 42, 33};
+    vector<int> numbers = {8, 3, 20, 56, 42, 33};
 
     linear_search(numbers, 42);
 
-	return 0;
+    return 0;
 }

cd31c7de9b3c75d8c9f07309bbffcce22c29fa5d
Josef Veselý <49988965+JosefVesely@users.noreply.github.com>
2020-05-23 20:20:05 +0200

    Create reverse_string.cpp

---
+++ reverse_string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+string reverse(string str)
+{
+    string reversed;
+
+    for (int i = str.length()-1; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    return reversed;
+}
+
+
+int main() {
+    string word;
+    cin >> word;
+
+    cout << reverse(word);
+    return 0;
+}
